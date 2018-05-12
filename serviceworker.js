f7c7b6201d52f7b9cf2d81e6418ed2a4e9c5cd67
Chelsea31 <s.bansal31@gmail.com>
2018-05-12 14:50:12 +0530

    changes to service worker

--- serviceworker.js
+++ serviceworker.js
@@ -1,19 +1,30 @@
 let version = "v1::";
+let CACHE_FILES = [
+    '/',
+    '/index.html',
+    '/restaurant.html',
+    '/css/styles.css',
+    '/js/dbhelper.js',
+    '/js/main.js',
+    '/data/restaurants.json',
+    '/js/restaurant_info.js',
+    '/img/1.jpg',
+    '/img/2.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
+    '/img/6.jpg',
+    '/img/7.jpg',
+    '/img/8.jpg',
+    '/img/9.jpg',
+    '/img/10.jpg'
+];
 
-self.addEventListener("install", function (event) {
+self.addEventListener('install', function (event) {
     console.log('WORKER : install in progress');
-    event.waitUntil(caches.open(version + 'restaurant').then(function (cache) {
-            return cache.addAll([
-                '/',
-                '/css/styles.css',
-                '/js/dbhelper.js',
-                '/js/main.js',
-                '/js/restaurant_info.js',
-                '/img/*',
-                '/data/restaurants.json',
-                '/index.html',
-                '/resaturant.html'
-            ])
+    event.waitUntil(
+        caches.open(version + 'restaurant').then(function (cache) {
+            return cache.addAll(CACHE_FILES);
         })
             .then(function () {
                 console.log("WORKER : install complete");
@@ -22,9 +33,9 @@ self.addEventListener("install", function (event) {
 });
 
 
-self.addEventListener("fetch", function (event) {
+self.addEventListener('fetch', function (event) {
     console.log("WORKER: fetch event in progress");
-    if (event.request.method !== 'get') {
+    if (event.request.method !== 'GET') {
         console.log("fetch event ignored" + event.request.method + event.request.url);
         return;
     }
@@ -33,14 +44,17 @@ self.addEventListener("fetch", function (event) {
                 .then(fetchedFromNetwork, unableToResolve)
                 .catch(unableToResolve);
             console.log('WORKER : fetch event ', cached ? '(cached)' : '(networked)', event.request.url);
-            return cached || netowrked;
+            return cached || networked;
 
             function fetchedFromNetwork(response) {
                 let cacheCopy = response.clone();
-                console("WORKER : fetch response from network", event.request.url);
+                console.log("WORKER : fetch response from network", event.request.url);
 
-                caches.open(version + 'pages').then(function add(cache) {
-                    console.log('WORKER: fetch response stored in cache.', event.request.url);
+                caches.open(version + 'restaurant').then(function add(cache) {
+                    cache.put(event.request, cacheCopy)
+                    .then(function () {
+                        console.log('WORKER: fetch response stored in cache.', event.request.url);
+                    })
                 });
                 return response;
             }
